a7a7de0d3c521823da0a58be0a5ccf70579b97fb
orselane <alexandremarques@cmail.carleton.ca>
2024-03-18 22:50:08 -0400

    Implemented main functions

--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 from config import load_config
 from connect import connect
 
+class DBAccessor():
+    def __init__(self):
+        self._db_cursor = connect(load_config()).cursor()
 
-def get_all_students():
-    """
-    Retrieves and displays all records from the students table.
-    """
-
-
-def add_student(first_name, last_name, email, enrollment_date):
-    """
-    Inserts a new student record into the students table.
-
-    :param first_name:
-    :param last_name:
-    :param email:
-    :param enrollment_date:
-    :return:
-    """
+    def get_all_students(self) -> list[tuple]:
+        """
+        Retrieves and displays all records from the students table.
+        """
+        self._db_cursor.execute("SELECT * FROM students")
+        return self._db_cursor.fetchall()
 
 
-def update_student_email(student_id, new_email):
-    """
-    Updates the email address for a student with the specified student_id.
+    def add_student(self, first_name, last_name, email, enrollment_date) -> None:
+        """
+        Inserts a new student record into the students table.
 
-    :param student_id:
-    :param new_email:
-    :return:
-     """
-
-
-def delete_student(student_id):
-    """
-    Deletes the record of the student with the specified student_id.
-
-    :param student_id:
-    :return:
-    """
+        :param first_name:
+        :param last_name:
+        :param email:
+        :param enrollment_date:
+        :return:
+        """
+        self._db_cursor.execute(f"INSERT INTO students (first_name, last_name, email, enrollment_date) "
+                                f"VALUES ('{first_name}', '{last_name}', '{email}', '{enrollment_date}')")
 
 
+    def update_student_email(self, student_id, new_email)  -> None:
+        """
+        Updates the email address for a student with the specified student_id.
+
+        :param student_id:
+        :param new_email:
+        :return:
+         """
+        self._db_cursor.execute(f"UPDATE students SET email = '{new_email}' WHERE student_id = {student_id} ")
+
+
+    def delete_student(self, student_id) -> None:
+        """
+        Deletes the record of the student with the specified student_id.
+
+        :param student_id: The student id of the student to delete.
+        """
+        self._db_cursor.execute(f"DELETE FROM students WHERE student_id = {student_id}")
+
+
+# Testing procedure, for your own tests, you can modify this file or initialize DBAccessor in an external test harness.
 if __name__ == '__main__':
-    sql_conn = connect(load_config())
+    accessor = DBAccessor()
+    # Base database
+    print(accessor.get_all_students())
+    # Student Alex should be added
+    accessor.add_student("Alex", "Marques", "a@ex.com", "01/01/2023")
+    print(accessor.get_all_students())
+    # Student John should be removed
+    accessor.delete_student(1)
+    print(accessor.get_all_students())
+    # Student Jane should have her email changed
+    accessor.update_student_email(2, "janeNewEmail@example.com")
+    print(accessor.get_all_students())
