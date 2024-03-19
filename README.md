d23945118b754064e45d7e5d82a8c0a81d3db4f9
orselane <alexandremarques@cmail.carleton.ca>
2024-03-18 21:55:59 -0400

    Adding assignment spec functions, and initial data (in readme)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # COMP3303A-A3-Q1
 Repo for COMP3303A Assignment 3 Question 1
+
+## Initial data
+CREATE TABLE students(
+	student_id serial PRIMARY KEY,
+	first_name varchar(25) NOT NULL,
+	last_name varchar(25) NOT NULL,
+	email varchar(25) NOT NULL UNIQUE,
+	enrollment_date date
+);
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
---
+++ main.py
@@ -0,0 +1,39 @@
+def get_all_students():
+    """
+    Retrieves and displays all records from the students table.
+    """
+
+
+def add_student(first_name, last_name, email, enrollment_date):
+    """
+    Inserts a new student record into the students table.
+
+    :param first_name:
+    :param last_name:
+    :param email:
+    :param enrollment_date:
+    :return:
+    """
+
+
+def update_student_email(student_id, new_email):
+    """
+    Updates the email address for a student with the specified student_id.
+
+    :param student_id:
+    :param new_email:
+    :return:
+     """
+
+
+def delete_student(student_id):
+    """
+    Deletes the record of the student with the specified student_id.
+
+    :param student_id:
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    print('PyCharm')
